c9749a618038b84342eb61ad25233214689d8aa3
David Boddie <david.boddie@canonical.com>
2023-02-17 15:35:20 +0100

    Imported an existing theme.

--- conf.py
+++ conf.py
@@ -48,6 +48,7 @@ source_suffix = {
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes"]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
