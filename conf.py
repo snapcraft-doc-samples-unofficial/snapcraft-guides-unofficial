fd866f99d6d98a7f2c30fef5430fb563022bdc18
David Boddie <david.boddie@canonical.com>
2023-02-17 15:59:15 +0100

    Enabled GitHub source links.

--- conf.py
+++ conf.py
@@ -54,3 +54,10 @@ html_theme_path = ["_themes"]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_context = {
+    'display_github': True,
+    'github_user': 'snapcraft-doc-samples-unofficial',
+    'github_repo': 'snapcraft-guides-unofficial',
+    'github_version': 'main/'
+}
